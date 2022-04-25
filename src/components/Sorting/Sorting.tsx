0d3b39f096a39b9724f226b73a044c1b7b34e428
Shovon <shovon@cuttingroom>
2022-04-25 19:36:30 +0600

    #5 The main component or the home component The main component is basically a canvas like for the numbers to be sorted

--- src/components/Sorting/Sorting.tsx
+++ src/components/Sorting/Sorting.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Bar from "./Bar";
 import "./style.scss";
 
-const NUMBERS = [50, 45, 75, 80, 50, 45, 75, 80];
+const NUMBERS = [50, 45, 75, 50, 80, 80, 50, 45, 75, 80, 18];
 const Sorting = () => {
+    const [numbers, setNumbers] = useState(NUMBERS);
+
     return (
         <div className="sorting">
-            {NUMBERS.map((number, index) => (
+            {numbers.map((number, index) => (
                 <Bar number={number} className={`bar${index}`} />
             ))}
         </div>
