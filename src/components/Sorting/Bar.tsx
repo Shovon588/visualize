acb865458b28dcee29ffaeb80b989ee189fd33dd
Shovon <shovon@cuttingroom>
2022-04-25 19:35:23 +0600

    #5 The bar component that will visualize the representation of each bar

--- src/components/Sorting/Bar.tsx
+++ src/components/Sorting/Bar.tsx
@@ -6,7 +6,14 @@ interface Props {
 }
 
 const Sorting: React.FC<Props> = ({ className = "", number }) => {
-    return <div className={`${className}`} style={{ height: `${number}%` }} />;
+    return (
+        <div className={`${className}`} style={{ height: `${number}%` }}>
+            <div className="bar-span">
+                <span>{number}</span>
+            </div>
+            <div className="bar"></div>
+        </div>
+    );
 };
 
 export default Sorting;
